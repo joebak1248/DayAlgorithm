c26cf5314e24db57c7a9b92f831db03bc26c7fde
joebak1248 <joebak@naver.com>
2020-07-20 16:28:12 +0900

    Update 2진수_큰숫자_비교_코드.java

--- DFS/2진수_큰숫자_비교_코드.java
+++ DFS/2진수_큰숫자_비교_코드.java
@@ -6,6 +6,8 @@ import java.util.*;
 import java.util.stream.IntStream;
 
 /**
+ * 기존적으론 DFS 문제이지만, visited 로 거르기 + 탐색할 배열들만 모아서 재귀 태우기로 응용을 해야만 풀 수 있는 문제였다.
+ *
  * n자리의 2진수를 표시하는 배열이 있다.
  * 이 정수형 배열은 '1' 이 마킹되는 2진수자릿수를 나타낸다.
  *  ex> [5, 4, 1] = 11001
@@ -39,12 +41,12 @@ public class Solution {
 			}
 		}
 
-//		int[][] sample = {{5, 4, 1}, {1 , 3, 4, 5}, {1,2,3}, {3,4,5,6}};
+//		int[][] sample = {{10, 4}, {10, 3}, {6, 5}};
 		int[] answer = solution(sample, SIZE);
 
-//		for (int i=0; i<answer.length; i++) {
-//			System.out.print(answer[i] + " ");
-//		}
+		for (int i=0; i<answer.length; i++) {
+			System.out.print(answer[i] + " ");
+		}
 	}
 
 	public int[] solution(int[][] arr, int size) {
@@ -54,8 +56,8 @@ public class Solution {
 			queue.addAll(toList(value));
 			allSortedList.add(queue);
 
-//			queue.forEach(v -> System.out.print(v + " "));
-//			System.out.println(" <<< VALUE ");
+			queue.forEach(v -> System.out.print(v + " "));
+			System.out.println(" <<< VALUE ");
 		}
 
 
@@ -76,26 +78,26 @@ public class Solution {
 		return answer;
 	}
 
-	private int getMaxIndex(List<List<Integer>> allSortedList, List<Integer> searchList, int compareIndex, List<Integer> visitedIndexList) {
+	private int getMaxIndex(List<List<Integer>> allSortedList, List<Integer> indexList, int depth, List<Integer> visitedIndexList) {
 		List<Integer> maxIndexList = new ArrayList<>();
 		int maxValue = -1;
-		for (int searchIndex : searchList) {
-			if (visitedIndexList.contains(searchIndex)) {
+		for (int index : indexList) {
+			if (visitedIndexList.contains(index)) {
 				continue;
 			}
 
-			int value = allSortedList.get(searchIndex).get(compareIndex);
+			int value = allSortedList.get(index).get(depth);
 			if (value > maxValue) {
 				maxIndexList.clear();
-				maxIndexList.add(searchIndex);
+				maxIndexList.add(index);
 				maxValue = value;
 			} else if (value == maxValue) {
-				maxIndexList.add(searchIndex);
+				maxIndexList.add(index);
 			}
 		}
 
 		if (maxIndexList.size() > 1) {
-			return getMaxIndex(allSortedList, maxIndexList, compareIndex+1, visitedIndexList);
+			return getMaxIndex(allSortedList, maxIndexList, depth+1, visitedIndexList);
 		}
 
 		return maxIndexList.get(0);
