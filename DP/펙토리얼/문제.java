3ab6a788f2da893f13551ea472f2cb2593a29f19
joebak1248 <joebak@naver.com>
2020-07-21 09:21:53 +0900

    Create 문제.java

---
+++ DP/펙토리얼/문제.java
@@ -0,0 +1,20 @@
+/**
+ * 펙토리얼
+ *
+ * @author Joebak
+ *
+ */
+public class Solution {
+
+	@Test
+	public void aa() {
+		int answer = solution(30);
+		System.out.print(answer);
+		Assert.assertEquals(832040, answer);
+	}
+
+	public int solution(int fibonacci) {
+		int answer = 0;
+		return answer;
+	}
+}
