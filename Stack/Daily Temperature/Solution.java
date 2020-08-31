ab026432477b03010eb169ec747826b9f8d8038f
joebak1248 <joebak@naver.com>
2020-08-31 16:13:21 +0900

    Create Solution.java

---
+++ Stack/Daily Temperature/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] dailyTemperatures(int[] T) {
+        int[] result = new int[T.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i=T.length-1; i>=0; i--) {
+            while (!stack.isEmpty() && T[stack.peek()] <= T[i]) {
+                stack.pop();   
+            }
+            
+            if (stack.isEmpty()) {
+                result[i] = 0;
+            } else {
+                result[i] = stack.peek() - i;
+            }
+            
+            stack.add(i);
+        }
+        return result;
+    }
+}
